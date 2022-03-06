e9ec39cb42b6636627b0a5adec317fbb48d94960
ferbushhh <43097781+ferbushhh@users.noreply.github.com>
2022-03-06 18:25:52 +0300

    update test.py
    fixed name def (test_*)

--- tests.py
+++ tests.py
@@ -16,28 +16,28 @@ def run_c(test_input):
     return output
 
 
-def correctTestFirst():
+def test_with_decimals():
     outputFirst = run_c(['BYN', '258.9'])
     assert outputFirst[0] == "Enter the name of the currency you want to convert from (USD, EUR, BYN, KZT): "
     assert outputFirst[1] == "Enter the amount to be converted: "
     assert re.fullmatch("Total in rubles ([0-9]+.)?[0-9]+ RUB.", outputFirst[2])
 
 
-def correstTestSecond():
+def test_with_integer():
     outputFirst = run_c(['USD', '3900'])
     assert outputFirst[0] == "Enter the name of the currency you want to convert from (USD, EUR, BYN, KZT): "
     assert outputFirst[1] == "Enter the amount to be converted: "
     assert re.fullmatch("Total in rubles ([0-9]+.)?[0-9]+ RUB.", outputFirst[2])
 
 
-def wrongTestFirst():
+def test_wrong_comma():
     outputFirst = run_c(['EUR', '3900,99'])
     assert outputFirst[0] == "Enter the name of the currency you want to convert from (USD, EUR, BYN, KZT): "
     assert outputFirst[1] == "Enter the amount to be converted: "
     assert outputFirst[2] == "!Wrong amount!"
 
 
-def wrongTestSecond():
+def test_wrong_o():
     outputFirst = run_c(['EUR', '39OO.99'])  # вместо 0 буква O
     assert outputFirst[0] == "Enter the name of the currency you want to convert from (USD, EUR, BYN, KZT): "
     assert outputFirst[1] == "Enter the amount to be converted: "
@@ -45,7 +45,7 @@ def wrongTestSecond():
 
 
 
-def wrongTestThird():
+def test_wrong_name_currency():
     outputFirst = run_c(['EURO', '39.99'])  # вместо EUR - EURO
     assert outputFirst[0] == "Enter the name of the currency you want to convert from (USD, EUR, BYN, KZT): "
     assert outputFirst[1] == "!Wrong format!"
