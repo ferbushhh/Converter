2c353c58150c38f5c53b63e94d5a8b6eb68c698d
ferbushhh <43097781+ferbushhh@users.noreply.github.com>
2022-03-06 18:19:21 +0300

    update tests.py
    fixed

--- tests.py
+++ tests.py
@@ -2,7 +2,8 @@ import re
 import main
 
 
-def test(data):
+def run_c(test_input):
+    data = test_input
     output = []
 
     def _input(item):
@@ -16,28 +17,28 @@ def test(data):
 
 
 def correctTestFirst():
-    outputFirst = test(['BYN', '258.9'])
+    outputFirst = run_c(['BYN', '258.9'])
     assert outputFirst[0] == "Enter the name of the currency you want to convert from (USD, EUR, BYN, KZT): "
     assert outputFirst[1] == "Enter the amount to be converted: "
     assert re.fullmatch("Total in rubles ([0-9]+.)?[0-9]+ RUB.", outputFirst[2])
 
 
 def correstTestSecond():
-    outputFirst = test(['USD', '3900'])
+    outputFirst = run_c(['USD', '3900'])
     assert outputFirst[0] == "Enter the name of the currency you want to convert from (USD, EUR, BYN, KZT): "
     assert outputFirst[1] == "Enter the amount to be converted: "
     assert re.fullmatch("Total in rubles ([0-9]+.)?[0-9]+ RUB.", outputFirst[2])
 
 
 def wrongTestFirst():
-    outputFirst = test(['EUR', '3900,99'])
+    outputFirst = run_c(['EUR', '3900,99'])
     assert outputFirst[0] == "Enter the name of the currency you want to convert from (USD, EUR, BYN, KZT): "
     assert outputFirst[1] == "Enter the amount to be converted: "
     assert outputFirst[2] == "!Wrong amount!"
 
 
 def wrongTestSecond():
-    outputFirst = test(['EUR', '39OO.99'])  # вместо 0 буква O
+    outputFirst = run_c(['EUR', '39OO.99'])  # вместо 0 буква O
     assert outputFirst[0] == "Enter the name of the currency you want to convert from (USD, EUR, BYN, KZT): "
     assert outputFirst[1] == "Enter the amount to be converted: "
     assert outputFirst[2] == "!Wrong amount!"
@@ -45,6 +46,6 @@ def wrongTestSecond():
 
 
 def wrongTestThird():
-    outputFirst = test(['EURO', '39.99'])  # вместо EUR - EURO
+    outputFirst = run_c(['EURO', '39.99'])  # вместо EUR - EURO
     assert outputFirst[0] == "Enter the name of the currency you want to convert from (USD, EUR, BYN, KZT): "
     assert outputFirst[1] == "!Wrong format!"
